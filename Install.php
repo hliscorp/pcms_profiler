e401b694304195481c7bd6b374c53de9f06dceb6
liviu@hliscorp.com <liviu@hliscorp.com>
2018-11-26 09:57:08 +0200

    Included Resources.php

--- Install.php
+++ Install.php
@@ -7,6 +7,7 @@ $include = preg_replace("/\\\\/", "/", dirname(dirname(dirname(__FILE__))));
 require_once "$include/application/models/InstallInterface.php";
 require_once "$include/application/models/dao/Resources.php";
 require_once "$include/application/models/query/Query.php";
+require_once "$include/application/models/query/Resources.php";
 
 /**
  * Created by PhpStorm.
